95607ec8ec73f93f2026c79c2f6393404afc4056
Kauan Lima <kauanalmeidalima1405@gmail.com>
2024-10-21 22:58:13 -0300

    Iniciando os estudos com Switch Case

--- AprendaAProgramarEmJavaComOO/primeiroprojeto/src/Main.java
+++ AprendaAProgramarEmJavaComOO/primeiroprojeto/src/Main.java
@@ -12,8 +12,17 @@ public class Main {
         double media = (9.8 + 6.3 + 8.0) /3;
         System.out.println(media);
         String sinopse;
-        sinopse = "Filme de aventura com galã dos anos 80";
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito Bom"
+                Ano de Lancamento
+                """ + anoDeLancamento;
+
         System.out.println(sinopse);
+
+        int classificacao = (int) (media/2);
+        System.out.println(classificacao);
 
     }
 }
