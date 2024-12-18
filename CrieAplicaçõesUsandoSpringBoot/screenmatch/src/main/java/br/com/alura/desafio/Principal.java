156ea06ca8bc5d0ac1f1076acdb039849a8384da
Kauan Lima <kauanalmeidalima1405@gmail.com>
2024-12-17 23:53:07 -0300

    Realizando desafio de Streams

---
+++ CrieAplicaçõesUsandoSpringBoot/screenmatch/src/main/java/br/com/alura/desafio/Principal.java
@@ -0,0 +1,16 @@
+package br.com.alura.desafio;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+
+public class Principal implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Principal.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
