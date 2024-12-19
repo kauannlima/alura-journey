db3f168b6392ed8278b4967b89735e24f0556b21
Kauan Lima <kauanalmeidalima1405@gmail.com>
2024-12-18 23:56:20 -0300

    Comitando alterações pendentes antes do rebase

---
+++ CrieAplicaçõesUsandoSpringBoot/3355-java-screenmatch-com-jpa-main/src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -0,0 +1,11 @@
+package br.com.alura.screenmatch.service;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+
+public class ConsultaChatGPT {
+    public static String obterTraducao(String texto) {
+
+        return texto;
+    }
+}
