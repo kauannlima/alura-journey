620c38d5852ddc701c77fa2225d440e70389e891
Kauan Lima <kauanalmeidalima1405@gmail.com>
2025-05-28 23:25:39 -0300

    Finalizando o CRUD da API com Mongo e Express

---
+++ Node.jsComExpress/express-mongo/src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import routes from "./routes/index.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+  console.error("Erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+  console.log("Conexão com o banco feita com sucesso");
+});
+
+const app = express();
+routes(app);
+
+app.delete("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).send("Livro removido com sucesso!");
+});
+
+export default app;
